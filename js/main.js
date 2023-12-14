04eb31f164dd1489d0286e9a8e973041891b3107
WolkovP <152427804+WolkovP@users.noreply.github.com>
2023-12-14 16:20:59 +0300

    Ready with adaptive.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function(){
     slidesToShow: 4,
     variableWidth: true,
     prevArrow: '<img class="arrow arrow-left" src="images/Arrow left.svg" alt="images">',
-    nextArrow: '<img class="arrow arrow-right" src="images/Arrow right.svg" alt="images">'
+    nextArrow: '<img class="arrow arrow-right" src="images/Arrow right.svg" alt="images">',
+    responsive: [
+      {
+        breakpoint: 361,
+        setiings: {
+          variableWidth: false,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
